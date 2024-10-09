589355dab30743b1f88bd608fcf6955385e232a5
rkhapov <r.khapov@ya.ru>
2024-10-09 08:28:14 +0000

    ht_hints.c: remove useless comment
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- src/ht_hints.c
+++ src/ht_hints.c
@@ -21,21 +21,6 @@ static void empty_stack_location_hint(__attribute__((unused)) void **begin,
 
 __thread stack_hintfn_t stack_hintfn = empty_stack_location_hint;
 
-// static stack_hintfn_t get_stack_hintfn() {
-//     if (__builtin_expect(stack_hintfn_status == HINTFN_UNINITIALIZED, 0)) {
-//         stack_hintfn = (stack_hintfn_t) dlsym(RTLD_DEFAULT, "ht_stack_location_hint");
-//         if (stack_hintfn == NULL) {
-//             __attribute__((unused)) char *msg = dlerror();
-//             stack_hintfn = empty_stack_location_hint;
-//         }
-//         stack_hintfn_status = HINTFN_INITIALIZED;
-
-//         return stack_hintfn;
-//     }
-
-//     return stack_hintfn;
-// }
-
 void ht_call_stack_location_hint(void **begin, void **end)
 {
 	stack_hintfn(begin, end);
